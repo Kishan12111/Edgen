273c5c8464aa93bb37203f6fdf50eb6464308dfd
Kishan12111 <1nt23ee026.kishan@nmit.ac.in>
2025-09-19 17:35:50 +0530

    first commit

---
+++ app/_component/Landingpg.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import { BackgroundLines } from "@/components/ui/background-lines";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+
+
+
+function Landingpg() {
+    const words = [
+    {
+      text: "Your ",
+    },
+    {
+      text: "Journey",
+    },
+    {
+      text: "Begins ",
+    },
+    {
+      text: "Here ",
+    },
+     {
+      text: "with ",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+    {
+      text: "Edgen! ",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <div>
+      <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          <TypewriterEffectSmooth words={words} />
+        </h2>
+
+        <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center text-semibold">
+        A One-Stop Personalized Career & Education Advisor
+
+        </p>
+      </BackgroundLines>
+    </div>
+  );
+}
+
+export default Landingpg;
